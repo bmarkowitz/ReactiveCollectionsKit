03a18459f6e1b12a0e8beac0f7f76d3366301fe8
Brett Markowitz <batting1000@comcast.net>
2024-10-26 15:35:09 -0400

    Create cell view model

---
+++ Example/Sources/Grid/PlanetCellViewModelGrid.swift
@@ -0,0 +1,39 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//  Documentation
+//  https://jessesquires.github.io/ReactiveCollectionsKit
+//
+//  GitHub
+//  https://github.com/jessesquires/ReactiveCollectionsKit
+//
+//  Copyright © 2019-present Jesse Squires
+//
+
+import ReactiveCollectionsKit
+import UIKit
+
+struct PlanetCellViewModelGrid: CellViewModel {
+    let planet: PlanetModel
+
+    // MARK: CellViewModel
+
+    var id: UniqueIdentifier { self.planet.id }
+
+    let contextMenuConfiguration: UIContextMenuConfiguration?
+
+    func configure(cell: GridColorCell) {
+        cell.label.text = self.planet.name
+    }
+
+    // MARK: Hashable
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.planet)
+    }
+
+    static func == (left: Self, right: Self) -> Bool {
+        left.planet == right.planet
+    }
+}
